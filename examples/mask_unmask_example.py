752ef0b589e9833705656d4b307912b469f27baf
Fahad <fsdqui@gmail.com>
2020-04-03 17:23:42 +0500

    Done

--- examples/mask_unmask_example.py
+++ examples/mask_unmask_example.py
@@ -15,5 +15,5 @@ client = PolyClient(api_token=client_token)
 masked_data = client.mask_data(data=input_data, key=masking_key)
 print(masked_data.get_text())
 
-unmasked_data = client.unmask_data(data=masked_data.text, key=masking_key)
+unmasked_data = client.unmask_data(data=masked_data.get_text(), key=masking_key)
 print(unmasked_data.get_text())
