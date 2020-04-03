b7a5c707df459e3cf494b91eaae0d469dc4836d0
Fahad <fsdqui@gmail.com>
2020-04-03 16:59:42 +0500

    Initial Commit

---
+++ examples/mask_unmask_example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from polysdk import PolyClient
+
+# you can generate it from "Developer API" under user icon
+client_token = "<your_api_token_here>"
+
+# masking key can be less than or equal to 16 characters
+masking_key = "<your_masking_key_here>"
+
+input_data = "Hey a@b.com!"
+
+client = PolyClient(api_token=client_token)
+
+masked_data = client.mask_data(data=input_data, key=masking_key)
+print(masked_data.get_text())
+
+unmasked_data = client.unmask_data(data=masked_data.text, key=masking_key)
+print(unmasked_data.get_text())
