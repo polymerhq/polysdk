ba9b53f0e18e4a081f1c747c4eae23d9323b73d9
Fahad <fsdqui@gmail.com>
2020-05-13 22:25:50 +0500

    v0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="polysdk",
-    version="0.0.6",
+    version="0.0.7",
     author="Fahad Siddiqui",
     author_email="fsid@predictdata.io",
     description="Polymer Python SDK.",
